a40c220a8217166b34220b627355d25a33e7660e
CptConi <nicolas.renard.33@gmail.com>
2020-10-13 15:11:48 +0200

    fisrt commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="search">
+    <button>Envoi de la requête</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      search:''
+    }
+  },
 }
 </script>
